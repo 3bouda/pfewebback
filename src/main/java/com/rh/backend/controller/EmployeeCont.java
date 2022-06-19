dad4c9bc121fba728f241b5bb5221295001ed431
3bouda <abdelaliiheb@gmail.com>
2022-06-19 18:20:15 +0100

    edem

--- src/main/java/com/rh/backend/controller/EmployeeCont.java
+++ src/main/java/com/rh/backend/controller/EmployeeCont.java
@@ -2,13 +2,21 @@ package com.rh.backend.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.rh.backend.model.Employee;
 import com.rh.backend.repo.EmployerRepo;
 import com.rh.backend.service.ImageService;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -119,14 +127,51 @@ public class EmployeeCont {
 
     void sendEmail(String email,String motDePasse) {
 
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(email);
+        
+        final String username = "skytek461@gmail.com";
+        final String password = "sdywconvwioflqmc";
 
-        msg.setSubject("Bienvenue dans notre équipe");
-        msg.setText("Nous sommes très heureux de vous avoir dans notre groupe !\n Nous croyons que vous pouvez utiliser vos compétences et vos talents pour aider notre entreprise à atteindre de nouveaux sommets. \n votre mot de passe est:"+motDePasse+"\n Vous pouvez connecté à notre app.\n Bienvenue à bord ! ");
+        // SimpleMailMessage msg = new SimpleMailMessage();
+        // msg.setFrom("skytek461@gmail.com");
+        // msg.setTo(email);
 
-        javaMailSender.send(msg);
+        // msg.setSubject("Bienvenue dans notre équipe");
+        // msg.setText("Nous sommes très heureux de vous avoir dans notre groupe !\n Nous croyons que vous pouvez utiliser vos compétences et vos talents pour aider notre entreprise à atteindre de nouveaux sommets. \n votre mot de passe est:"+motDePasse+"\n Vous pouvez connecté à notre app.\n Bienvenue à bord ! ");
 
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.starttls.port","587");
+
+        prop.put("mail.smtp.connectiontimeout","5000");
+        prop.put("mail.smtp.timeou","5000");
+        prop.put("mail.smtp.writetimeout","5000");
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+                try {
+
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress("skytek461@gmail.com"));
+                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+                    message.setSubject("Bienvenue dans notre équipe");
+                    message.setText("Dear Mail Crawler,"
+                            + "\n\n Please do not spam my email!");
+        
+                    Transport.send(message);
+        
+                    System.out.println("Done");
+        
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
     }
 
 
