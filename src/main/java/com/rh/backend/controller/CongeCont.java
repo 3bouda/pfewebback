973b534e3da3988e2a26cafaad7d468852276239
3bouda <abdelaliiheb@gmail.com>
2022-06-20 15:01:00 +0100

    ken rahou abdou

--- src/main/java/com/rh/backend/controller/CongeCont.java
+++ src/main/java/com/rh/backend/controller/CongeCont.java
@@ -7,11 +7,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,23 +54,17 @@ public class CongeCont {
         return congeRepo.save(conge);
     }
     
-    @PutMapping("/{id}")
-    Conge update(@PathVariable String id,@RequestBody Conge conge){
+    @GetMapping("accept/{id}")
+    Conge accept(@PathVariable String id){
         Conge  congeFromDB = congeRepo.findById(id).orElseThrow(RuntimeException::new);
-        congeFromDB.setIdemploye(conge.getIdemploye());
-        congeFromDB.setDescription(conge.getDescription());
-        congeFromDB.setDateDebut(conge.getDateDebut());
-        congeFromDB.setDateFin(conge.getDateFin());
-        congeFromDB.setType(conge.getType());
-        congeFromDB.setPreuve(conge.getPreuve());
-        congeFromDB.setEtat(conge.getEtat());
-
+        congeFromDB.setEtat("accept");
         return congeRepo.save(congeFromDB);
     }
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void delete(@PathVariable String id){
-        Conge congeToDelete = congeRepo.findById(id).orElseThrow(RuntimeException::new);
-        congeRepo.delete(congeToDelete);
+
+    @GetMapping("refus/{id}")
+    Conge refus(@PathVariable String id){
+        Conge  congeFromDB = congeRepo.findById(id).orElseThrow(RuntimeException::new);
+        congeFromDB.setEtat("refus");
+        return congeRepo.save(congeFromDB);
     }
 }
